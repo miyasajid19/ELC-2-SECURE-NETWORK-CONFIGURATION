01dfef68d9cde6a5f402812e4da1d0c1e8ecca03
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-12-10 15:31:51 +0530

    Ceaser Cipher

---
+++ ceaserCipher.py
@@ -0,0 +1,32 @@
+import numpy as np
+def caesar_encrypt(text, shift):
+    encrypted = ""
+    for char in text:
+        if char.isalpha():
+            shift_base = 65 if char.isupper() else 97
+            encrypted += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            encrypted += char
+    return encrypted
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
+# Main Function
+if __name__ == "__main__":
+    try:
+        text="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        caesar_shift=26
+
+        # Encrypt and Decrypt with Caesar Cipher
+        caesar_encrypted =caesar_encrypt( text, caesar_shift)
+        caesar_decrypted = caesar_decrypt( caesar_encrypted, caesar_shift)
+        print("Original Text ::",text)
+        print("Caeser Shift ::",caesar_shift)
+        print("Encrypted Text ::",caesar_encrypted)
+        # Verification (Match Texts)
+        print("Verification Results:")
+        print(f"Caesar Cipher Match: {text == caesar_decrypted}")
+
+    except FileNotFoundError:
+        print("The file 'Text_To_Be_Encrypted.txt' was not found. Please ensure it exists in the script's directory.")
